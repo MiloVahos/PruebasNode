1958892be5c7c0e5b52d5b3177cf78bfc8ff3eda
Juan Camilo Peña Vahos <jpena@cidenet.com.co>
2019-01-29 10:53:46 -0500

    Avances

--- 04-todo/app.js
+++ 04-todo/app.js
@@ -1,14 +1,27 @@
 const argv = require('./config/yargs').argv;
+const porHacer = require('./por-hacer/por-hacer');
+const colors = require('colors');
 
 let comando = argv._[0];
 
 switch ( comando ) {
 
     case 'crear':
-        console.log(' Crear por hacer ');
+        let tarea = porHacer.crear( argv.descripcion );
+        console.log(tarea);
     break;
-
     case 'listar':
+
+        let listado = porHacer.getListado();
+
+        for ( let tarea of listado ) {
+            console.log('=============POR HACER============'.green);
+            console.log(tarea.descripcion);
+            console.log('Estado: ', tarea.completado);
+            console.log('=================================='.green);
+
+        }
+
         console.log(' Mostrar todas las tareas por hacer ');
     break;
 
---
+++ 04-todo/por-hacer/por-hacer.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+
+    let data = JSON.stringify(listadoPorHacer);
+    fs.writeFile('db/data.json',data, (err) => {
+        if (err) throw new Error('No se pudo grabar',err);
+    });
+
+}
+
+const cargarDB = () => {
+
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+
+}
+
+const crear = (descripcion) => {
+
+    cargarDB();
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+    guardarDB();
+    return porHacer;
+
+}
+
+const getListado = () => {
+    cargarDB();
+    return listadoPorHacer;
+}
+
+module.exports = {
+    crear,
+    getListado
+}
