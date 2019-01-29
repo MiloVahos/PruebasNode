e7cc333e019190377be37e5811a4736641ee969c
Juan Camilo Peña Vahos <jpena@cidenet.com.co>
2019-01-29 11:25:28 -0500

    Avances

--- 04-todo/app.js
+++ 04-todo/app.js
@@ -13,20 +13,18 @@ switch ( comando ) {
     case 'listar':
 
         let listado = porHacer.getListado();
-
         for ( let tarea of listado ) {
             console.log('=============POR HACER============'.green);
             console.log(tarea.descripcion);
             console.log('Estado: ', tarea.completado);
             console.log('=================================='.green);
-
         }
 
-        console.log(' Mostrar todas las tareas por hacer ');
     break;
 
     case 'actualizar':
-        console.log(' Actualiza una tarea por hacer ');
+        let actualizado = porHacer.actualizar(argv.descripcion,argv.completado);
+        console.log(actualizado);
     break;
 
     default:
--- 04-todo/por-hacer/por-hacer.js
+++ 04-todo/por-hacer/por-hacer.js
@@ -40,7 +40,22 @@ const getListado = () => {
     return listadoPorHacer;
 }
 
+const actualizar = ( descripcion, completado = true ) => {
+
+    cargarDB();
+    let index = listadoPorHacer.findIndex( tarea => tarea.descripcion === descripcion );
+    if ( index >= 0 ) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
 module.exports = {
     crear,
-    getListado
+    getListado,
+    actualizar
 }
