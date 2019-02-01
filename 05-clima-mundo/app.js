08c12b7093b267c36f3d380e1a8c36a049356d5c
Juan Camilo Peña Vahos <jpena@cidenet.com.co>
2019-02-01 13:45:14 -0500

    Empieza app del clima

---
+++ 05-clima-mundo/app.js
@@ -0,0 +1,21 @@
+const argv = require('yargs').options({ 
+    direccion: {
+        alias: 'd',
+        desc: ' Dirección de la ciudad para obtener el clima',
+        demmand: true
+    }
+}).argv;
+
+console.log( argv.direccion );
+
+// AIzaSyDyJPPlnIMOLp20Ef1LlTong8rYdTnaTXM
+// AIzaSyDzbQ_553v-n8QNs2aafN9QaZbByTyM7gQ
+// AIzaSyA5mjCwx1TRLuBAjwQw84WE6h5ErSe7Uj8
+// AIzaSyCroCERuudf2z02rCrVa6DTkeeneQuq8TA
+// AIzaSyBkDYSVRVtQ6P2mf2Xrq0VBjps8GEcWsLU
+// AIzaSyAu2rb0mobiznVJnJd6bVb5Bn2WsuXP2QI
+// AIzaSyAZ7zantyAHnuNFtheMlJY1VvkRBEjvw9Y
+// AIzaSyDSPDpkFznGgzzBSsYvTq_sj0T0QCHRgwM
+// AIzaSyD4YFaT5DvwhhhqMpDP2pBInoG8BTzA9JY
+// AIzaSyAbPC1F9pWeD70Ny8PHcjguPffSLhT-YF8
+// https://developers.google.com/maps/documentation/geocoding/start
