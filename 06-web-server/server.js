e8ae681312f716ea5e50497cf57d5dc422e30468
Juan Camilo Peña Vahos <jpena@cidenet.com.co>
2019-02-13 16:43:35 -0500

    Avances

--- 06-web-server/server.js
+++ 06-web-server/server.js
@@ -1,16 +1,43 @@
-var express = require('express')
-var app = express();
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
 
-app.use( express.static( __dirname + '/public' ) )
+app.use( express.static( __dirname + '/public' ) );
+// Express HBS
+hbs.registerPartials( __dirname + '/views/parciales' );
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getAnio', () => {
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper('capitalizar', (texto) => {
+  let palabras = texto.split(' ');
+  palabras.forEach( (palabra, idv) => {
+    palabras[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+  });
+  return palabras.join(' ');
+
+});
 
 app.get('/', (req, res) => {
   // res.send('Hello World')
-    let salida = {
+    /*let salida = {
         nombre: 'Camilo',
         edad: 23,
         url: req.url
-    }
-    res.send(salida);
+    }*/
+    //res.send(salida);
+    res.render('Home', {
+      nombre: 'Camilo',
+      anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+      anio: new Date().getFullYear()
+    });
 });
  
 app.listen(3000)
