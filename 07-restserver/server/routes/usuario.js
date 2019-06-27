9dbb19fa76104090ee24876efd21383540ff0cf0
Camilo Vahos <jpena@cidenet.com.co>
2019-06-27 15:48:00 -0500

    Avances en el servidor rest

---
+++ 07-restserver/server/routes/usuario.js
@@ -0,0 +1,66 @@
+const express = require('express');
+
+const bcrypt = require('bcrypt');
+
+const _ = require('underscore');
+
+const Usuario = require('../models/usuario');
+
+const app = express();
+
+app.get('/usuario', function(req,res) {
+  let desde = req.query.desde || 0;
+  desde = Number(desde);
+  let limite = req.query.limite || 5;
+  limite = Number(limite);
+
+  Usuario.find({  }).skip(desde).limit(limite).exec( (err, usuarios) => {
+    if (err) {
+      return res.status(400).json({ ok: false, err });
+    }
+    res.json({
+      ok: true,
+      usuarios
+    })
+  });
+});
+
+app.post('/usuario', function(req,res) {
+  let body = req.body;
+  let usuario = new Usuario({
+    nombre: body.nombre,
+    email: body.email,
+    password: bcrypt.hashSync(body.password,10),
+    role: body.role
+  });
+
+  usuario.save( (err, usuarioDB) => {
+    if (err) {
+      return res.status(400).json({ ok: false, err, mensaje: 'El nombre es necesario' })
+    }
+    res.json({ ok: true, usuario: usuarioDB })
+  });
+  
+});
+
+app.put('/usuario/:id', function(req, res) {
+
+  let id = req.params.id;
+  let body = _.pick(req.body, ['nombre','email','img','role','estado']);
+
+  Usuario.findByIdAndUpdate( id, body, { new: true, runValidators: true }, (err,usuarioDB) => {
+
+    if (err) {
+      return res.status(400).json({ ok: false, err})
+    }
+    res.json({ ok: true, usuario: usuarioDB })
+
+  });
+
+});
+
+app.delete('/usuario', function(req, res) {
+  req.json('Delete usuario');
+});
+
+module.exports = app;
--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -1,5 +1,8 @@
 require('./config/config');
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -7,24 +10,13 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req,res) {
-  res.json('Get usuario');
+app.use(require('./routes/usuario'));
+
+mongoose.connect('mongodb://localhost:27017/cafe', (err, resp) => {
+  if ( err ) throw err;
+  console.log('Base de datos ONLINE');
 });
 
-app.post('/usuario/:id', function(req,res) {
-  let id = req.params.id;
-  let body = req.body;
-
-  if ( body.Nombre === undefined ) {
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesario'
-    })
-  } else {
-    res.json({id, body});
-  }
-  
-});
 
 app.listen(process.env.PORT, () => {
   console.log('Escuchando puerto', process.env.PORT);
